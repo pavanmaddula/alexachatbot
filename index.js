6bf68aac194fa965ff521e91ad9ff2e82820debe
pavanmaddula <31445693+pavanmaddula@users.noreply.github.com>
2018-08-30 15:34:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,86 +1,109 @@
 
-		'use strict';
-		const request = require("request");
-		const Alexa=require('alexa-sdk');
-		var rows,rows1,speecht='hi';
-		var listarray = [];
-		 
-		const APP_ID='amzn1.ask.skill.de8e5974-f9f7-4031-a3b8-d6a82785ddad';
-		const key = "1PSRyQ2nKESErf2xicokVGuoO1sQclll7ZGLiIs-7q_U";
-		 const url = "https://spreadsheets.google.com/feeds/list/"+key+"/od6/public/values?alt=json";
-		exports.handler = function(event, context, callback) {
-		  
-		  const alexa=Alexa.handler(event,context,callback);
-		  alexa.APP_ID=APP_ID;
-		  alexa.registerHandlers(handlers);
-		  alexa.execute();
+			'use strict';
+			const request = require("request");
+			const Alexa=require('alexa-sdk');
+			var rows,rows1,speecht='hi';
+			var listarray = [],listarray2=[];
+			 
+			const APP_ID='amzn1.ask.skill.de8e5974-f9f7-4031-a3b8-d6a82785ddad';
+			const key = "1PSRyQ2nKESErf2xicokVGuoO1sQclll7ZGLiIs-7q_U";
+			 const url = "https://spreadsheets.google.com/feeds/list/"+key+"/od6/public/values?alt=json";
+			 
+			exports.handler = function(event, context, callback) {
+			  
+			  const alexa=Alexa.handler(event,context,callback);
+			  alexa.APP_ID=APP_ID;
+			  alexa.registerHandlers(handlers);
+			  alexa.execute();
 
-		};
-		var handlers={
+			};
+            
 
-			'LaunchRequest':function(){
-				this.emit(':ask','this is  the response from google sheets kindly say hi');
-			},
-			'googlesheet':function(event){
-				request({
-		    json: true,
-		    url: url
-		  }, function (error, response, body) {
-		    if (error || response.statusCode !== 200) return
+			var handlers={
 
-		    let parsed = body.feed.entry.map( (entry) => {
-		    	        
-		       
-		      rows = entry.title["$t"]
+				'LaunchRequest':function(){
+					this.emit(':ask','welcome to Aditya institutions,these are the events which is conducted by technical hub . if your willing to listen ,kindly say yes or else say no');
+				},
 
-		       listarray.push(rows);
-		      // console.log(listarray);
+				'aboutevents':function(event){
+	               
+	               
+	            request({
+			    json: true,
+			    url: url
+			    }, function (error, response, body) {
+			    if (error || response.statusCode !== 200) return
 
-		       // "updated": entry.title[1]
-		       
-               //console.log(typeof(rows));
-		      
-               
-		      // Dynamically add all relevant columns from the Sheets to the response
-		     /* Object.keys( entry ).forEach( (key) => {
-		        if ( /gsx\$/.test(key) ) {
-		          let  newKey = key.replace("gsx$", "");
-		          rows[newKey] = entry[key]["$t"];
-		          
+			    let parsed = body.feed.entry.map( (entry) => {
+			    	        
+			       
+			      rows = entry.title["$t"]
 
-		          var speechOutput=rows[newKey];
-		         
-		          speecht=speechOutput;
-		           }
-		     return row		      });*/
-		      //console.log(rows);
-		      
-		     
-		 })
-		console.log('this is the output from list array',listarray[0]);
-		
-		      
-		 });
-				
-				
-		        this.emit(':ask',listarray);
-	            listarray=[];
+			       listarray.push(rows);
+			       
+			       
+			      })
+			});
+					
+					
+			        this.emit(':ask',listarray);
+			       
+			        listarray=[];
+			        
+			       
+		            
 
-		 },
-			'AMAZON.HelpIntent': function () {
-		        const speechOutput = HELP_MESSAGE;
-		        const reprompt = HELP_REPROMPT;
+			     },
 
-		        this.response.speak(speechOutput).listen(reprompt);
-		        this.emit(':responseReady');
-		    },
-		    'AMAZON.CancelIntent': function () {
-		        this.response.speak(STOP_MESSAGE);
-		        this.emit(':responseReady');
-		    },
-		    'AMAZON.StopIntent': function () {
-		        this.response.speak(STOP_MESSAGE);
-		        this.emit(':responseReady');
-		    },
 
-		};
+			    'allevents':function(){
+			    	var eventtime=this.event.request.intent.slots.time.value;
+			    	if(eventtime==='at a time'){
+			    		this.emit(':ask',listarray);
+			    	}
+			    	if(eventtime==='individual'){
+			    		this.emit('googlesheet');
+			    	}
+
+			    },
+
+			    'googlesheet':function(){
+
+			    	
+			     var eventdetails=this.event.request.intent.slots.eventname.value;
+			             for(var i=1;i<=listarray.length;i++){
+			                 if( eventdetails==listarray[i]){	
+							this.emit(':ask',listarray[i+1]);
+							
+		                       }
+		                     }
+			   
+			   console.log(listarray);
+			       
+			   },
+			 
+			    'Unhandled':function(){
+			    	this.emit(':ask','sorry i dont no that one');
+			    },
+			    'AMAZON.NoIntent':function(){
+			    	this.emit(':ask','thanks for listening . you can ask anything about on going events .i am here to help you. ');
+			    },
+			    
+				'AMAZON.HelpIntent': function () {
+			        const speechOutput = HELP_MESSAGE;
+			        const reprompt = HELP_REPROMPT;
+
+			        this.response.speak(speechOutput).listen(reprompt);
+			        this.emit(':responseReady');
+			    },
+			    'AMAZON.CancelIntent': function () {
+			        this.response.speak(STOP_MESSAGE);
+			        this.emit(':responseReady');
+			    },
+			    'AMAZON.StopIntent': function () {
+			        this.response.speak(STOP_MESSAGE);
+			        this.emit(':responseReady');
+			    },
+			   
+
+			};
